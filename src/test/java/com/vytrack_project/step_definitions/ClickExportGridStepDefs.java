b0c412d260e4473dc374e4e6ceedf65df6408983
olha0139 <olga-d7@outlook.com>
2022-07-11 15:43:44 -0400

    updated ClickExportGridStepDefs

--- src/test/java/com/vytrack_project/step_definitions/ClickExportGridStepDefs.java
+++ src/test/java/com/vytrack_project/step_definitions/ClickExportGridStepDefs.java
@@ -8,14 +8,18 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
-
-import java.util.Map;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ClickExportGridStepDefs {
 
     VyTrackLoginPage vyTrackLoginPage = new VyTrackLoginPage();
     VyTrackExportGridPage vyTrackExportGridPage = new VyTrackExportGridPage();
-
+    int winHeight, winWidth, xPos, yPos;
+    WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
 
     @Given("{string} is on Vehicles page")
     public void isOnVehiclesPage(String user) {
@@ -28,7 +32,11 @@ public class ClickExportGridStepDefs {
 
     @When("user clicks on Export Grid dropdown")
     public void user_clicks_on_export_grid_dropdown() {
+        WebElement loader = Driver.getDriver().findElement(By.cssSelector("div[class='loader-mask shown']"));
+        wait.until(ExpectedConditions.invisibilityOf(loader));
         vyTrackExportGridPage.exportGridDropdown.click();
+        wait.until(ExpectedConditions.invisibilityOf(loader));
+
     }
 
     @Then("user should be able to see CSV and XLSX")
@@ -38,6 +46,23 @@ public class ClickExportGridStepDefs {
         Assert.assertEquals("XLSX", vyTrackExportGridPage.XLSXGrid.getText());
 
     }
+    @When("user looks for Export Grid dropdown")
+    public void user_looks_for_export_grid_dropdown() {
+
+        int winHeight = Driver.getDriver().manage().window().getSize().getHeight();
+        int winWidth = Driver.getDriver().manage().window().getSize().getWidth();
+        int xPos = vyTrackExportGridPage.exportGridDropdown.getLocation().getX();
+        int yPos = vyTrackExportGridPage.exportGridDropdown.getLocation().getY();
+
+    }
+    @Then("user should be able to see Export Grid dropdown on the left of the page")
+    public void user_should_be_able_to_see_export_grid_dropdown_on_the_left_of_the_page() {
+
+        Assert.assertTrue((xPos <= winWidth/2) && (yPos <= winHeight/2));
+    }
+
+
+
 
 
 }
